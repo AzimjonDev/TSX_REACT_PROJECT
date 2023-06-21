a4186fff61f5e1ad3c0758b25b257f38c67090c2
azimjon <azimjonqoshoqov@gmail.com>
2023-06-22 00:14:50 +0500

    finish code

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Incrment from "./components/incrment";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Incrment />
+  </React.StrictMode>
+);
