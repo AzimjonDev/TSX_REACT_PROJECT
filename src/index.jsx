d664bef65f7039ab89664f8844ff30fa1e06d45f
azimjon <azimjonqoshoqov@gmail.com>
2023-06-22 23:47:13 +0500

    d

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Incrment from "./components/incrment";
+import App from "./app";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Incrment />
-  </React.StrictMode>
+	<React.StrictMode>
+		<div>
+			<App />
+		</div>
+	</React.StrictMode>
 );
