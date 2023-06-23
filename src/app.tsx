0e9c61aae470ad0d5a2cfe4e20497e77e5c9ec32
azimjon <azimjonqoshoqov@gmail.com>
2023-06-23 14:01:56 +0500

    tr

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,8 @@
-import { json } from "node:stream/consumers";
 import React, { Component } from "react";
 import Board from "./components/board/board";
 import Histories from "./components/histories";
 
-type Player = "X" | "O";
+type Player = "X" | "O" | "";
 type TBoard = (string | null)[];
 export interface AppState {
 	board: TBoard;
@@ -11,55 +10,96 @@ export interface AppState {
 	nextPlayer: Player;
 	winner?: Player;
 	currentIdx: number;
-
+	game: boolean;
 }
 
 export default class App extends Component<{}, AppState> {
-
-	constructor(props={}){
+	constructor(props = {}) {
 		super(props);
-		this.state = JSON.parse(localStorage.getItem("state")!)|| {
+		this.state = JSON.parse(localStorage.getItem("state")!) || {
 			board: new Array(9).fill(null),
 			histories: [new Array(9).fill(null)],
 			nextPlayer: "X",
 			currentIdx: 0,
-			// winner: "X"
+			winner: "",
+			game: true,
 		};
 	}
-	setStatStorage:typeof this.setState=(state)=>{
-		this.setState(state,()=>{
-			localStorage.setItem("state", JSON.stringify(this.state))
-		})
-	}
+	setStatStorage: typeof this.setState = (state) => {
+		this.setState(state, () => {
+			localStorage.setItem("state", JSON.stringify(this.state));
+		});
+	};
+	checkWinner = () => {
+		const win = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+
+		for (let i = 0; i < win.length; i++) {
+			const [a, b, c] = win[i];
+			if (
+				this.state.board[a] !== null &&
+				this.state.board[a] === this.state.board[b] &&
+				this.state.board[a] === this.state.board[c]
+			) {
+				return true;
+			}
+		}
+		return false;
+	};
 
 	handleCell = (idx: number) => {
 		let { nextPlayer, histories, currentIdx } = this.state;
 		let board = [...this.state.board];
 
 		if (board[idx]) return;
-
-		board[idx] = nextPlayer;
-		nextPlayer = nextPlayer === "X" ? "O" : "X";
-		this.setStatStorage((prev) => ({
-			board,
-			nextPlayer,
-			histories: [...histories, board],
-			currentIdx: currentIdx + 1,
-		}));
+		if (this.state.game) {
+			board[idx] = nextPlayer;
+			nextPlayer = nextPlayer === "X" ? "O" : "X";
+			this.setStatStorage((prev) => ({
+				board,
+				nextPlayer,
+				histories: [...histories, board],
+				currentIdx: currentIdx + 1,
+			}));
+		}
 	};
-	handleHistores=(idx:number) =>{
-		const {histories}=this.state;
-		const board=[...histories[idx]]
-		this.setStatStorage({board,currentIdx: idx})
-	}
+
+	checkwin = () => {
+		const win = this.checkWinner();
+		if (win) {
+			// this.state.game = false;
+			this.setState({ game: false });
+			let value = this.state.nextPlayer === "X" ? "O" : "X";
+			return <p style={{ color: "green" }}>{value} is winner 🏆</p>;
+		}
+		return <p>No winner !!!</p>;
+	};
+	handleHistores = (idx: number) => {
+		const { histories } = this.state;
+		const board = [...histories[idx]];
+		this.setStatStorage({ board, currentIdx: idx });
+	};
 	render() {
-		const { board, nextPlayer, histories, currentIdx, } = this.state;
+		const { board, nextPlayer, histories, currentIdx } = this.state;
 		return (
 			<main>
 				<div className="container d-flex " style={{ gap: 10 }}>
 					<Board board={board} onCell={this.handleCell} />
 
-					<Histories onHistory={this.handleHistores} currentIdx={currentIdx} nextPlayer={nextPlayer} histories={histories} />
+					<Histories
+						onHistory={this.handleHistores}
+						currentIdx={currentIdx}
+						nextPlayer={nextPlayer}
+						histories={histories}
+					/>
 				</div>
 			</main>
 		);
